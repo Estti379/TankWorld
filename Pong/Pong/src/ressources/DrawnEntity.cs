bcc8876e4869fc4814de7401f1ac97dbd82d50d0
Esteves Tiago <estti379@gmail.com>
2019-04-25 18:09:03 +0200

    Entity creation (GameEntity+)
    -Created IUpdateEntity
    -Created GameEntity
    -Created DrawnEntity
    -Created Paddle object

---
+++ Pong/Pong/src/ressources/DrawnEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pong.src.ressources
+{
+    abstract public class DrawnEntity
+    {
+        private Dictionary<string, SpriteEntity> sprites;
+
+        //Constructors
+        public DrawnEntity()
+        {
+            sprites = new Dictionary<string, SpriteEntity>();
+        }
+        
+        //Accessors
+        public Dictionary<string, SpriteEntity> AllSprites
+        {
+            get{ return sprites; }
+        }
+
+        //Methods
+        abstract public void EntityRender(IntPtr render);
+
+        public void AddSprite(string key, SpriteEntity newSprite)
+        {
+            if (newSprite.Texture == IntPtr.Zero)
+            {
+                Console.Write("Sprite has no texture: "+ key +"\n");
+            }
+            else if (sprites.ContainsKey(key))
+            {
+                Console.Write("Key already eist in dictionnary: " + key + "\n");
+            }
+            else
+            {
+                sprites.Add(key, newSprite);
+            }
+        }
+
+        public void RemoveSprite(string key)
+        {
+                sprites.Remove(key);
+        }
+
+    }
+}
---
+++ Pong/Pong/src/ressources/Items/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static SDL2.SDL;
+
+namespace Pong.src.ressources.Items
+{
+    class Paddle : DrawnEntity
+    {
+
+        //Constructors
+        public Paddle() : base()
+        {
+            
+        }
+
+        //Accessors
+
+
+        //Methods
+        override public void EntityRender(IntPtr render)
+        {
+            foreach (SpriteEntity current in AllSprites.Values)
+            {
+                SDL_RenderCopy(render, current.Texture, ref current.SubRect, ref current.Pos);
+            }
+        }
+    }
+}
